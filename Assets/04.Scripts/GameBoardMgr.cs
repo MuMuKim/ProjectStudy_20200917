cab23f0e2a3ce067a719547f1bd92074f31886b5
MuMuKim <67031535+MuMuKim@users.noreply.github.com>
2020-09-23 19:13:35 +0900

    레이에 맞은 생성된 큐브의 색만 바꾼다.

--- Assets/04.Scripts/GameBoardMgr.cs
+++ Assets/04.Scripts/GameBoardMgr.cs
@@ -16,7 +16,7 @@ public class GameBoardMgr : MonoBehaviour
     public float rayDistance = 10.0f;
     public LayerMask gridLayer;
 
-    //grid 위에 생성한 큐브
+    //grid 위에 생성한 가이드 큐브
     public GameObject guideCube;
 
     // 스크립트
@@ -33,16 +33,23 @@ public class GameBoardMgr : MonoBehaviour
             if (hit.collider.CompareTag("CUBE"))
             {
                 guideCube.SetActive(false); // 가이드 큐브를 꺼놓는다.
+                if(bottonMgr.obj == hit.collider.gameObject)
+                {
+                    hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.cyan;
+                }
+                else
+                {
+                    bottonMgr.obj.GetComponent<MeshRenderer>().material.color = Color.red;
+                }
 
-
-                Debug.Log("1층");
-                Debug.Log($"히트노말{hit.normal}");
+                // Debug.Log("1층");
+                // Debug.Log($"히트노말{hit.normal}");
 
                 // Hit된 노말백터가 백터3 up(y)과 같을 때 (X) 이유 : 월드가 아님
                 // Hit된 노말백터와 Hit된 오브젝트의 로컬up방향과 같을 때 (O)
                 if (hit.normal == hit.transform.up.normalized)
                 {
-                    Debug.Log("2층");
+                    // Debug.Log("2층");
                     guideCube.SetActive(true);
 
                     guideCube.transform.position = hit.transform.GetChild(0).gameObject.transform.position;
