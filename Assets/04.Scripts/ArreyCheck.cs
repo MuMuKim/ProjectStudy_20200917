5181fb448ed61c3fba01ca8daec66975aa8c8a2c
MuMuKim <67031535+MuMuKim@users.noreply.github.com>
2020-10-05 00:55:10 +0900

    20201005 정답테스트 (검출과 비교는 되나, 카운트 조절과 가끔 검출이 안됨)

---
+++ Assets/04.Scripts/ArreyCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArreyCheck : MonoBehaviour
+{
+    public GameObject[] rayIndex;
+    public GameObject[] answerIndex = new GameObject[9];
+
+    int cnt = 0;
+
+    public void CheckCube()
+    {
+        for (int i = 0; i < rayIndex.Length; i++)
+        {
+            Ray ray = new Ray(rayIndex[i].gameObject.transform.position,
+                                rayIndex[i].gameObject.transform.forward* 10f);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit,10f,1<<8))
+            {
+                print("검출" + rayIndex[i]);
+                // answerIndex[0] = hit.collider.gameObject;
+
+                if (rayIndex[i].layer == answerIndex[i].layer)
+                {
+                    ++cnt;
+                    print("카운트" +cnt);
+
+                    if (cnt == 3f)
+                    {
+                        print("정답");
+                    }
+                }
+            }
+        }
+    }
+    
+}
